21f50ed96ff2f8ea8d941e750e3912c8198d8185
Cornelis de Mooij <codemo@gmail.com>
2019-03-06 15:38:49 +0100

    Added caching

--- fe-guild-2019-pwa/sw.js
+++ fe-guild-2019-pwa/sw.js
@@ -1,6 +1,32 @@
+const CACHE_STATIC_NAME = 'static';
+const URLS_TO_PRECACHE = [
+  '/',
+  'index.html',
+  'src/js/app.js',
+  'src/js/feed.js',
+  'src/lib/material.min.js',
+  'src/lib/material.indigo-deep_orange.min.css',
+  'src/css/app.css',
+  'src/css/feed.css',
+  'src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  // 'https://code.getmdl.io/1.3.0/material.indigo-deep_orange.min.css"'
+];
+
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing Service Worker ...', event);
-  event.waitUntil(self.skipWaiting());
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(cache => {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll(URLS_TO_PRECACHE);
+      })
+      .then(() => {
+        console.log('[ServiceWorker] Skip waiting on install');
+        return self.skipWaiting();
+      })
+  );
 });
 
 self.addEventListener('activate', event => {
@@ -8,14 +34,42 @@ self.addEventListener('activate', event => {
   return self.clients.claim();
 });
 
+// Add a new cache for dynamic content:
+const CACHE_DYNAMIC_NAME = 'dynamic';
+
 self.addEventListener('fetch', event => {
   console.log('[Service Worker] Fetching something ...', event);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log(response);
+          return response;
+        }
 
-  // This fixes a weird bug in Chrome when you open the Developer Tools:
-  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-    return;
-  }
+        // Clone the request - a request is a stream and can be only consumed once:
+        const requestToCache = event.request.clone();
 
-  event.respondWith(fetch(event.request));
-})
+        // Try to make the original HTTP request as intended:
+        return fetch(requestToCache)
+          .then(response => {
+            // If request fails or server responds with an error code, return that error immediately:
+            if (!response || response.status !== 200) {
+              return response;
+            }
 
+            // Again clone the response because you need to add it into the cache and because it's used
+            // for the final return response:
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_DYNAMIC_NAME)
+              .then(cache => {
+                cache.put(requestToCache, responseToCache);
+              });
+            
+            return response;
+          })
+      })
+      .catch(error => console.log('[Service Worker] Dynamic cache error.', error))
+  );
+});
